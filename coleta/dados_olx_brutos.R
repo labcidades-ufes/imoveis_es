b987acf3eac4b4f468b83bbe934c4f9bb3562e47
Arthur-110 <arthur@Arthur.localdomain>
2026-07-07 07:34:27 -0300

    mudanças na coleta

--- coleta/dados_olx_brutos.R
+++ coleta/dados_olx_brutos.R
@@ -292,11 +292,12 @@ collect_raw_data <- function() {
     #-------------------------------------------------------------------------------------------------------------------------------------------
     # Dados de coletas anteriores
     anuncios_olx_anteriores <- read_latest_parquet_from_minio("bronze/imoveis_es/municipal/") 
-    if(anuncios_olx_anteriores %>% nrow() ==0){
+    if(anuncios_olx_anteriores %>% nrow() == 0){
+        anuncios_olx_filtrados <- anuncios_olx_anteriores
+    }else { 
         anuncios_olx_filtrados <- anuncios_olx_anteriores %>%
-        filter(!(is.null('preco_R$') & is.null(area_m2) & is.null(n_quartos) & is.null(n_banheiros) & is.null(n_vagas_garagem) & is.null(endereco) & is.null(cep) ))
-    
-    }else { anuncios_olx_filtrados <- anuncios_olx_anteriores}
+        filter(!(is.na(`preco_R$`) & is.na(area_m2) & is.na(n_quartos) & is.na(n_banheiros) & is.na(n_vagas_garagem) & is.na(endereco) & is.na(cep) ))
+    }
    
     anuncios_pendentes <- anuncios_olx %>% anti_join(anuncios_olx_filtrados, by = "url")
     
